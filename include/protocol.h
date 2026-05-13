e66881cb17e3ff1f1273a88202ba1582ed3cf895
Ian Shi <ianshi1111@gmail.com>
2026-05-13 15:10:04 -0400

    restructing message struct

--- include/protocol.h
+++ include/protocol.h
@@ -5,50 +5,63 @@
 #include <vector>
 #include <cstdint>
 #include <fstream>
+using UnixNanos = int64_t;
 
+// visualization of a message format
+// [4 bytes: path_len][path_len bytes: path string][4 bytes: body_len][body_len bytes: body string][N bytes: mtime]
+
+// note: path should be less than 4096 bytes following unix max path size
 struct Message{
-    uint8_t opcode; // what happened (create, modify, delete, etc)
+    Opcode opcode; // what happened (create, modify, delete, etc)
     std::string path; // file which was changed
     std::vector<char> body; // file content, will be empty for deletes
-    int64_t mtime; // modification time
+    UnixNanos mtime; // modification time
 
 };
 
-enum opcode : uint8_t{
+enum class Opcode : uint8_t{
     FILE_CREATE = 0x01,
     FILE_MODIFY = 0x02,
     FILE_DELETE = 0x03,
 };
 
-std::string serialize(const Message& msg){
+// seperate check since message is a struct
+bool isValidMessage(const Message& msg){
+    if (msg.opcode == Opcode::FILE_DELETE && !msg.body.empty()){
+        return false;
+    }
+    if (msg.path.empty() || msg.path.size() > 4096){
+        return false;
+    }
+    return true
+}
+
+int serialize(const Message& msg){
     // turns message to raw bytes to send
-    fout.open("output.bin", ios::out | std::ios::binary);
+    std::ofstream fout;
+    fout.open("output.bin", std::ios::out | std::ios::binary);
     if (!fout) {
         std::cerr << "Error opening file for writing." << std::endl;
-        return "";
+        return 1;
     }
     if (msg.opcode == FILE_CREATE || msg.opcode == FILE_MODIFY){
-        // serialize opcode, path, body, and mtime
-        fout.write(reinterpret_cast<const char*>(&msg.opcode), sizeof(msg.opcode));
 
-        uint32_t path_len = msg.path.size();
-        // need write path length to prevent stuff like \n in the path messing it up
-        fout.write(reinterpret_cast<const char*>(&path_len), sizeof(path_len));
-        fout.write(reinterpret_cast<const char*>(msg.path.c_str()), path_len);
-
-        uint32_t body_len = msg.body.size();
-        fout.write(reinterpret_cast<const char*>(&body_len), sizeof(body_len));
-        fout.write(reinterpret_cast<const char*>(msg.body.c_str(), body_len));
-
-        fout.write(reinterpret_cast<const char*>(&msg.mtime), sizeof(msg.mtime));
 
     } else if (msg.opcode == FILE_DELETE){
-        // serialize opcode, path, and mtime (body will be empty)
-        fout.read(reinterpret_cast<const char*>())
+
     };
     f.close()
+    return 0
 };
 
 std::string deserialize(const Message& msg){
     // turns raw bytes to readable message
+    std::ifstream fin;
+    fin.open("input.bin", std::ios::in | stds::ios::binary);
+    if (!fin) {
+        std::cerr << "Error opening file for reading." << std::endl;
+        return "";
+    }
+    fin.read
+    
 };
